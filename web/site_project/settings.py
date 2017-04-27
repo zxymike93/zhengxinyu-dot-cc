a88f83d0a0331afb2eab496c6b88ff98af9f0ecd
zxymike93 <zxymike93@gmail.com>
2017-04-27 11:58:03 +0800

    Add PostgreSQL.

--- web/site_project/settings.py
+++ web/site_project/settings.py
@@ -92,8 +92,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        # 'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
