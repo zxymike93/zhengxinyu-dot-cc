8777f5371f72bd54cc6b954630693e83926b987d
zxymike93 <zxymike93@gmail.com>
2017-05-04 00:02:40 +0800

    Test for blog's home page -- list func.

--- web/blog/tests.py
+++ web/blog/tests.py
@@ -1,3 +1,19 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
-# Create your tests here.
+from .views import list
+
+
+class UrlTest(TestCase):
+
+    def test_root_url_resolve_to_list(self):
+        found = resolve('/')
+        self.assertEqual(found.func, list)
+
+    def test_list_return_html(self):
+        req = HttpRequest()
+        resp = list(req)
+        self.assertTrue(resp.content.startswith(b'\n<!DOCTYPE html>'))
+        self.assertIn(b"<title>Mike's Blog</title>", resp.content)
+        self.assertTrue(resp.content.endswith(b'</html>\n'))
